785410ccda3cff9ee1e8f1be3746ada9434a58b3
Kosimi <kosimis@wit.edu>
2023-03-24 18:14:39 -0400

    Updated design , header is ready

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import portrait from "./img/portrait.jpg";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="box">
+        <div className="header">
+          <img
+            src={portrait}
+            alt="Aziza Kosimi"
+            className="header__image"
+          ></img>
+          <div className="header__heading">
+            <h2 className="header__heading--name">
+              Aziza <br />
+              Rajabova-Kosimi
+            </h2>
+            <h3 className="header__heading--position">
+              Founder & CEO of
+              <a href="d" className="header__heading--company">
+                "LLC Tojtex"
+              </a>
+            </h3>
+          </div>
+        </div>
+
+        <div className="body"></div>
+      </div>
+      <img src={portrait} alt="Aziza Kosimi" className="header__shadow"></img>
     </div>
   );
 }

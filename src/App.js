408d6aaf7e21ddb8c879111debe8ce2b117bf467
Kosimi <kosimis@wit.edu>
2023-03-25 13:19:18 -0400

    Initial design is finished, taplink is ready to use, one bug still exists (background photo)

--- src/App.js
+++ src/App.js
@@ -23,8 +23,62 @@ function App() {
             </h3>
           </div>
         </div>
-
-        <div className="body"></div>
+        <div className="footer">
+          <ul className="list">
+            <li className="list__item">
+              <div className="list__item--icon">
+                <i className="bx bx-phone"></i>
+              </div>
+              +992 90 446 7808
+            </li>
+            <li className="list__item">
+              <div
+                className="list__item--icon"
+                onClick={() => window.open("mailto:kosimi.a@tojtex.tj")}
+              >
+                <i class="bx bx-envelope"></i>
+              </div>
+              kosimi.a@tojtex.tj
+            </li>
+            <li className="list__item">
+              <div
+                className="list__item--icon"
+                onClick={() => window.open("https://www.tojtex.tj/", "_blank")}
+              >
+                <i class="bx bx-globe"></i>
+              </div>
+              www.tojtex.tj
+            </li>
+            <li className="list__item">
+              <div className="list__item--icon">
+                <i class="bx bx-map"></i>
+              </div>
+              2/1 Chorteppa st, Dushanbe
+            </li>
+            <li className="list__item">
+              <div
+                className="list__item--icon"
+                onClick={() =>
+                  window.open("https://www.instagram.com/aziza__r/", "_blank")
+                }
+              >
+                <i class="bx bxl-instagram"></i>
+              </div>
+              @aziza__r
+            </li>
+            <li className="list__item">
+              <div
+                className="list__item--icon"
+                onClick={() =>
+                  window.open("https://www.facebook.com/azizakosimi/", "_blank")
+                }
+              >
+                <i class="bx bxl-facebook"></i>
+              </div>
+              @azizakosimi
+            </li>
+          </ul>
+        </div>
       </div>
       <img src={portrait} alt="Aziza Kosimi" className="header__shadow"></img>
     </div>
